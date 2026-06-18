c7136dfa5e57cc2e2eac09ec9823c82d35759ce6
Harshit-sys169 <f20240474@pilani.bits-pilani.ac.in>
2026-06-18 20:28:14 +0530

    Add tab icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,33 @@
-import { Stack } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#208AEF",
+        tabBarInactiveTintColor: "#94A3B8",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
